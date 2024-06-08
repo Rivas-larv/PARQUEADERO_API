c7659a3262f2cbba3ce641182e84b15d9ea4f2ec
ARivas <alejandrorivas.larv@gmail.com>
2024-06-08 12:04:19 -0500

    ParqueaderoAPI-Modificación metodo HTTP update(put-->patch)

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -8,7 +8,7 @@ router.post('/', vehicleController.createVehicle);
 router.get('/', vehicleController.getVehicles);
 router.get('/:id', vehicleController.getVehicleById);
 router.put('/:id/exit', vehicleController.exitVehicle);
-router.put('/:id', vehicleController.updateVehicle);
+router.patch('/:id', vehicleController.updateVehicle);
 router.delete('/:id', vehicleController.deleteVehicle);
 //Exportar rutas
 module.exports = router;
